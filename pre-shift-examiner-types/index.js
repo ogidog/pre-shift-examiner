40d383b071d7d715c1de15fa5fa658a1556a51d1
ogidog <ogidog@mail.ru>
2023-02-06 00:27:42 +0700

    Add render.com postgres support

---
+++ pre-shift-examiner-types/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NotifierMessages = exports.ErrorMessages = void 0;
+exports.ErrorMessages = {
+    PERSONNEL_ID_ERROR: "Неправильный табельный номер",
+    SERVER_ERROR: "Ошибка сервера",
+};
+exports.NotifierMessages = {
+    AUTHENTICATION: "Аутентификация...",
+    TEST_LOADING: "Загрузка теста...",
+    CHECKING_ANSWERS: "Проверка ответов...",
+};
+//# sourceMappingURL=index.js.map
