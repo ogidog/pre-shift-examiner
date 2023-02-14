58b02cc379f4951df9f4dc94706362beb98b84e3
ogidog <ogidog@mail.ru>
2023-02-15 02:30:15 +0700

    Add LogoTitle component

--- pre-shift-examiner-middleware/shared/cookies/_at.ts
+++ pre-shift-examiner-middleware/shared/cookies/_at.ts
@@ -12,7 +12,7 @@ export class AccessTokenCookie {
             {expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN!}
         );
 
-        res.cookie("at", token, {
+        res.cookie("_at", token, {
             "secure": process.env.NODE_ENV === "production",
             "maxAge": parseInt(process.env.ACCESS_TOKEN_COOKIE_MAX_AGE!),
             "domain": "pre-shift-examiner-api.onrender.com",
